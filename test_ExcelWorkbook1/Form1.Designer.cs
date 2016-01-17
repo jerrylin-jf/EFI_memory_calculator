682a7ce16fda1c68a0b48c2919819211f81901af
GrayHui <linhuihui225@gmail.com>
2016-01-17 20:19:53 +0800

    basic convert

--- test_ExcelWorkbook1/Form1.Designer.cs
+++ test_ExcelWorkbook1/Form1.Designer.cs
@@ -33,26 +33,30 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Register_textBox1 = new System.Windows.Forms.TextBox();
+            this.Bus_textBox = new System.Windows.Forms.TextBox();
+            this.Devicd_textBox = new System.Windows.Forms.TextBox();
+            this.Offset_textBox = new System.Windows.Forms.TextBox();
+            this.Functino_label = new System.Windows.Forms.Label();
+            this.Function_textBox = new System.Windows.Forms.TextBox();
+            this.Register_textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 203);
+            this.button1.Location = new System.Drawing.Point(83, 142);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Location = new System.Drawing.Point(17, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 1;
@@ -61,7 +65,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Location = new System.Drawing.Point(17, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 2;
@@ -70,7 +74,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(95, 111);
+            this.label3.Location = new System.Drawing.Point(96, 75);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 3;
@@ -79,49 +83,86 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(187, 109);
+            this.label4.Location = new System.Drawing.Point(17, 104);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Offset:";
             // 
-            // textBox1
+            // Register_textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
+            this.Register_textBox1.Location = new System.Drawing.Point(72, 23);
+            this.Register_textBox1.MaxLength = 4;
+            this.Register_textBox1.Name = "Register_textBox1";
+            this.Register_textBox1.Size = new System.Drawing.Size(63, 20);
+            this.Register_textBox1.TabIndex = 5;
             // 
-            // textBox2
+            // Bus_textBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(46, 108);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(43, 20);
-            this.textBox2.TabIndex = 6;
+            this.Bus_textBox.Location = new System.Drawing.Point(47, 72);
+            this.Bus_textBox.Name = "Bus_textBox";
+            this.Bus_textBox.Size = new System.Drawing.Size(43, 20);
+            this.Bus_textBox.TabIndex = 7;
             // 
-            // textBox3
+            // Devicd_textBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(141, 108);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(38, 20);
-            this.textBox3.TabIndex = 7;
+            this.Devicd_textBox.Location = new System.Drawing.Point(141, 72);
+            this.Devicd_textBox.Name = "Devicd_textBox";
+            this.Devicd_textBox.Size = new System.Drawing.Size(38, 20);
+            this.Devicd_textBox.TabIndex = 8;
             // 
-            // textBox4
+            // Offset_textBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(231, 106);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(39, 20);
-            this.textBox4.TabIndex = 8;
+            this.Offset_textBox.Location = new System.Drawing.Point(61, 101);
+            this.Offset_textBox.Name = "Offset_textBox";
+            this.Offset_textBox.Size = new System.Drawing.Size(39, 20);
+            this.Offset_textBox.TabIndex = 11;
+            // 
+            // Functino_label
+            // 
+            this.Functino_label.AutoSize = true;
+            this.Functino_label.Location = new System.Drawing.Point(185, 75);
+            this.Functino_label.Name = "Functino_label";
+            this.Functino_label.Size = new System.Drawing.Size(51, 13);
+            this.Functino_label.TabIndex = 9;
+            this.Functino_label.Text = "Function:";
+            // 
+            // Function_textBox
+            // 
+            this.Function_textBox.Location = new System.Drawing.Point(236, 72);
+            this.Function_textBox.Name = "Function_textBox";
+            this.Function_textBox.Size = new System.Drawing.Size(43, 20);
+            this.Function_textBox.TabIndex = 10;
+            // 
+            // Register_textBox2
+            // 
+            this.Register_textBox2.Location = new System.Drawing.Point(141, 23);
+            this.Register_textBox2.MaxLength = 4;
+            this.Register_textBox2.Name = "Register_textBox2";
+            this.Register_textBox2.Size = new System.Drawing.Size(61, 20);
+            this.Register_textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Register_textBox2);
+            this.Controls.Add(this.Function_textBox);
+            this.Controls.Add(this.Functino_label);
+            this.Controls.Add(this.Offset_textBox);
+            this.Controls.Add(this.Devicd_textBox);
+            this.Controls.Add(this.Bus_textBox);
+            this.Controls.Add(this.Register_textBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -142,9 +183,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox Register_textBox1;
+        private System.Windows.Forms.TextBox Bus_textBox;
+        private System.Windows.Forms.TextBox Devicd_textBox;
+        private System.Windows.Forms.TextBox Offset_textBox;
+        private System.Windows.Forms.Label Functino_label;
+        private System.Windows.Forms.TextBox Function_textBox;
+        private System.Windows.Forms.TextBox Register_textBox2;
+        private System.Windows.Forms.Label label5;
     }
 }
--- test_ExcelWorkbook1/Form1.cs
+++ test_ExcelWorkbook1/Form1.cs
@@ -13,6 +13,9 @@ namespace test_ExcelWorkbook1
 {
     public partial class Form1 : Form
     {
+        public const int busWeight = 1048576;
+        public const int deviceWeight = 32768;
+        public const int functionWeight = 4096;
         public Form1()
         {
             InitializeComponent();
@@ -25,8 +28,25 @@ namespace test_ExcelWorkbook1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            uint registerDec = UInt32.Parse(Register_textBox1.Text+Register_textBox2.Text, System.Globalization.NumberStyles.HexNumber);
+            uint busDec= UInt32.Parse(Bus_textBox.Text, System.Globalization.NumberStyles.HexNumber);
+            uint deviceDec = UInt32.Parse(Devicd_textBox.Text, System.Globalization.NumberStyles.HexNumber);
+            uint functionDec = UInt32.Parse(Function_textBox.Text, System.Globalization.NumberStyles.HexNumber);
+            uint offsetDec = UInt32.Parse(Offset_textBox.Text, System.Globalization.NumberStyles.HexNumber);
+
+            uint addressDec = (busDec * busWeight) + (deviceDec * deviceWeight) + (functionDec * functionWeight) + offsetDec + registerDec;
+            string addressHex = Convert.ToString(addressDec, 16);
+
+            label5.Text = addressHex;
+     
             Excel.Worksheet wks = (Excel.Worksheet)Globals.ThisWorkbook.ActiveSheet;
-            wks.Range["A1"].Value = "Hello World";
+            wks.Range["A1"].Value = registerDec;
+            wks.Range["A2"].Value = busDec;
+            wks.Range["A3"].Value = deviceDec;
+            wks.Range["A4"].Value = functionDec;
+            wks.Range["A5"].Value = offsetDec;
+            wks.Range["A6"].Value = addressDec;
+            wks.Range["A7"].Value = addressHex;
         }
         
     }
