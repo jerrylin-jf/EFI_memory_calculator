83d05af4f1a6e8f060d7b90223f4f055e4794cb6
GrayHui <linhuihui225@gmail.com>
2016-01-21 17:06:27 +0800

    add Ribbon print hello world

---
+++ test_ExcelWorkbook1/Ribbon1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace test_ExcelWorkbook1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnConnectSerial_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Worksheet wks = (Excel.Worksheet)Globals.ThisWorkbook.ActiveSheet;
+            wks.Range["D1"].Value = "Hello world";
+        }
+    }
+}
